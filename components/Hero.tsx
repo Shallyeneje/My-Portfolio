a86a040dfa0c2d53fb9df198d1e93afb9f038219
Shallyeneje <Shalomenje2@gmail.com>
2026-06-12 12:33:59 +0100

    herotext

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,20 +8,19 @@ export default function Hero() {
       <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-14 items-center">
         {/* ✅ Left: Text Content */}
         <div className="text-center md:text-left">
-          <h2 className="text-xl md:text-2xl font-bold ">
-            Hello, 
+          <h2 className="text-xl md:text-2xl font-bold ">Hello,</h2>
+          <h2 className="font-bold text-xl md:text-3xl mb-2">
+            {" "}
+            I’m Eneje Chimcheberem Shalom
           </h2>
-          <h2 className="font-bold text-xl md:text-3xl mb-2"> I’m{" "}
-            Eneje Chimcheberem Shalom</h2>
-            <h2 className="font-bold text-xl md:text-3xl mb-6"> a{" "}
-            <span className="text-blue-500">Frontend Web Developer</span></h2>
+          <h2 className="font-bold text-xl md:text-3xl mb-6">
+            {" "}
+            a <span className="text-blue-500">Frontend Web Developer</span>
+          </h2>
           <p className="text-sm md:text-base max-w-2xl mb-6">
-            2+ years of experience
-            specializing in{" "}
-            <strong>Next.js, React, and TypeScript</strong>.<br/> I build
-            responsive, scalable, and engaging web apps — with growing
-            expertise in{" "}
-            <strong>React Native, and AI/ML integrations</strong>.
+            Frontend Engineer specializing in <strong>React, Next.js, and TypeScript.</strong> I
+            build responsive, scalable, and user-focused applications that
+            transform ideas into real-world digital solutions.
           </p>
 
           <div className="flex flex-wrap gap-4 justify-center md:justify-start">
